fac07184d5f06e772af8643ade8f46a8b74a6704
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-07-04 13:48:58 +0530

    Update Basic.sql

--- Basic.sql
+++ Basic.sql
@@ -1,3 +1,22 @@
 Create database github; /*  create database (database_name)*/
 /* you want see  how many database store */ show databases;
 
+/* how to create simple database and create simple table step by step*/
+/* step 01*/
+create database diru;
+/* step 02 use that database*/
+use diru;
+/* setp 03 create table*/
+create table datainfo ( 
+  id int primary key,
+  name varchar(10),
+  ); /* craete table (table name)*/
+/* you want add new column*/
+alter table datainfo add gpa int;/* alter table (table name) add (column name) data type */
+/* you want remove column */
+alter table datainto drop gap;
+/* you want view your table*/
+describe gpa /* describe table name*/
+/* insert ah value for table/*
+
+
